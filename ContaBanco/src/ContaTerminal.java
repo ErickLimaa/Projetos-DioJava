055f69607a958125eaffb278600c10185e3c3c04
ErickLimaa <erick.prudencio738@gmail.com>
2024-03-11 10:03:10 -0300

    Um projeto de uma conta no banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = scanner.nextInt();
+        System.out.println("Por favor, digite a Agência ! ");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite seu nome ! ");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite seu saldo ! ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por ciar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já esta disponível para saque");
+
+
+        
+    }
+}
